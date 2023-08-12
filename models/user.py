01d6e894e1b8188bcf69eaa4f500f04a1dda2e11
danieljonesobot <daniel.jonesobot@gmail.com>
2023-08-12 12:36:29 +0000

    created user class

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""In this script, we define class called User to inherits from 
+parent class BaseModel
+"""
+
+from base_model.py import BaseModel
+
+class User(BaseModel):
+    """constructor"""
+    def __init__(self):
+        self.email = ''
+        self.password = ''
+        self.first_name = ''
+        self.last_name = ''
+
+	
+
