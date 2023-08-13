be422d1a57e183b570548a133bd39650af46db74
Anthon002 <chineduanulugwo@gmail.com>
2023-08-13 05:18:33 -0700

    added count and all actions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 
+
 class FileStorage:
     """ this class stores the json to a file path
     Private attributes:
